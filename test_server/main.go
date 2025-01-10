2bdcc8e97c7e6a2d8548ee6849dcfd4a9e267346
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-01-10 16:39:33 +0530

    refactor: precalculate delay

--- test_server/main.go
+++ test_server/main.go
@@ -8,30 +8,35 @@ import (
 	"time"
 )
 
+type Server struct {
+	Delay int
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8081"
 	}
 	delay := os.Getenv("DELAY")
+	intDelay := 0
 	if delay == "" {
-		delay = "1000"
+		intDelay = 0
 	}
-	http.HandleFunc("/foo", fooHandler)
+	intDelay, err := strconv.Atoi(delay)
+	if err != nil {
+		fmt.Println("Error converting delay to int")
+	}
+	server := Server{Delay: intDelay}
+
+	http.HandleFunc("/foo", server.fooHandler)
+
 	fmt.Printf("starting a test backend server on port %s with delay %s", port, delay)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	delay := os.Getenv("DELAY")
-	if delay == "" {
-		delay = "1000"
+func (s *Server) fooHandler(w http.ResponseWriter, r *http.Request) {
+	if s.Delay > 0 {
+		time.Sleep(time.Duration(s.Delay) * time.Millisecond)
 	}
-	intDelay, err := strconv.Atoi(delay)
-	if err != nil {
-		panic(err)
-	}
-	time.Sleep(time.Duration(intDelay) * time.Millisecond)
-	fmt.Println("foo API called at", time.Now())
 	w.Write([]byte("foo"))
 }
